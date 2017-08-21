590003738a24ca7070ca50ade0966314801c5de0
Matthew Downs <mwdowns@gmail.com>
2017-08-21 11:38:13 -0400

    First commit, first simple code

---
+++ first-app.rb
@@ -0,0 +1,6 @@
+print "hello\n"
+print "what's your name?\n"
+name = gets.chomp
+print "what's your weight?\n"
+weight = gets.to_f
+print "your name is #{name} and your weight is ", weight * 0.92, " pounds on Saturn.\n"
