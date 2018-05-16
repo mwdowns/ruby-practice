910e0a51ac4acbf73a4c7a27f12f95bc8a71b266
mwdowns <mwdowns@gmail.com>
2018-05-16 14:27:48 -0400

    adds fizzbuzz.rb and code

---
+++ fizbuzz.rb
@@ -0,0 +1,38 @@
+def doFizzBuzz(num)
+    if checkFizzBuzz(num)
+        puts "fizbuzz"
+    elsif checkBuzz(num)
+        puts "buzz"
+    elsif checkFiz(num)
+        puts "fiz"
+    else
+        puts num
+    end
+end
+
+def checkFizzBuzz(num)
+    if num % 3 == 0 && num % 5 == 0
+        return true
+    else
+        return false
+    end
+end
+
+def checkBuzz(num)
+    if num % 5 == 0
+        return true
+    else
+        return false
+    end
+end
+
+def checkFiz(num)
+    if num % 3 == 0
+        return true
+    else
+        return false
+    end
+end
+
+numArr = (1...50).to_a
+numArr.map{|num| doFizzBuzz(num)}
