97eb3de3ba72465a7be276eb7cd019cc35dbc792
Matthew Downs <mwdowns@gmail.com>
2018-05-17 14:23:36 -0400

    adds some customization questions

--- filewrite.rb
+++ filewrite.rb
@@ -1,4 +1,9 @@
-file_name = "./sample.txt"
+puts "what what's the filename you'd like to create?\n"
+file_name = "./" << gets.chomp
+puts "what would you like to say?\n"
+str = gets.chomp
+puts "how many times would you like to say it?\n"
+num = gets.to_i
 def file_writer(file_name, str, num)
     line = str << "\n"
     File.open(file_name, 'r+') do |file_handle|
@@ -15,5 +20,5 @@ def file_line_counter(file_name)
     end
 end
 
-file_writer(file_name, "heyo, dude!", 5)
+file_writer(file_name, str, num)
 file_line_counter(file_name)
