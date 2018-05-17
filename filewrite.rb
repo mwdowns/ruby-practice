42c5278a2e3daeea21208180aa4bbd5d0c1a52f4
Matthew Downs <mwdowns@gmail.com>
2018-05-17 13:55:15 -0400

    changes the code a bit and adds a line counter

--- filewrite.rb
+++ filewrite.rb
@@ -1,4 +1,11 @@
 file_name = "./sample.txt"
-file_handle = File.open(file_name, 'w')
-file_handle << "yo, dude!"
-file_handle.close
+File.open(file_name, 'r+') do |file_handle|
+    10.times do |index|
+        line = "yo, dude!\n"
+        file_handle <<  line
+    end
+end
+File.open(file_name, 'r') do |file_handle|
+    arr = file_handle.read.split("\n").length
+    puts arr
+end
